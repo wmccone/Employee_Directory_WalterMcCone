8aca3206071674458bdc21729b57b663d8c4cb48
Walter M <waltermccone88@gmail.com>
2021-05-14 10:56:27 -0500

    added a sort function to utils and added sort functionality to my employee class

--- src/components/employeedata.js
+++ src/components/employeedata.js
@@ -4,7 +4,7 @@ function Employees(props) {
     return (
         <tbody>
         {props.employees.map(employee => (
-                <tr key={employee.id}>
+                <tr key={employee.login.uuid}>
                     <th scope="row"><img alt={employee.name.first} src={employee.picture.thumbnail} /></th>
                     <td>{employee.name.first}</td>
                     <td>{employee.email}</td>
--- src/components/main.js
+++ src/components/main.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 import EmployeeTable from "./employeetable";
 import Employees from "./employeedata";
 import API from "../utils/API";
+import compareValues from "../utils/sort";
 
 
 class MainEmployeeData extends Component {
   state = {
-    employees: {}
+    employees: []
   }
 componentDidMount() {
   this.createDirectory();
@@ -14,13 +15,31 @@ componentDidMount() {
 
 createDirectory = () => {
  API.getEmployees()
- .then(res => this.setState({employees: res.results}))
+ .then(res => {
+   console.log(res)
+   this.setState({employees: res.data.results})
+  })
  .catch(err => console.log(err));
 };
 
+sortAsc = (event) => {
+  const employeeArr = this.state.employees
+  let newOrder = employeeArr.sort(compareValues(event.target.value))
+  this.setState(newOrder)
+}
+
+sortDesc = (event) => {
+  const employeeArr = this.state.employees
+  let newOrder = employeeArr.sort(compareValues(event.target.value, 'desc'))
+  this.setState(newOrder)
+}
+
   render() {
     return <main className="main">
-      <EmployeeTable>
+      <EmployeeTable
+      sortAsc={this.sortAsc}
+      sortDesc={this.sortDesc}
+      >
         <Employees 
         employees={this.state.employees}
         />
